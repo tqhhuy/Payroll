2ea3f0edc8fe1a2fbf8fda7111a58e5eac90c3cf
SE140040\Minh Hieu <johnneo000@gmail.com>
2021-10-15 12:04:47 +0700

    HieuTCM update API

--- PayrollSystem/ClientApp/src/api/axiosClient.js
+++ PayrollSystem/ClientApp/src/api/axiosClient.js
@@ -1,6 +1,11 @@
 // api/axiosClient.js
 import axios from 'axios';
 import queryString from 'query-string';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import Authorise from './authorise.js';
 // Set up default config for http requests here
 
 // Please have a look at here `https://github.com/axios/axios#request-config` for the full list of configs
@@ -13,7 +18,12 @@ headers: {
 paramsSerializer: params => queryString.stringify(params),
 });
 axiosClient.interceptors.request.use(async (config) => {
-// Handle token here ...
+const currentUser = firebase.auth().currentUser;
+if(currentUser){
+    // var tokenTxt = ''; tokenTxt= currentUser.getIdToken();
+    // const response = Authorise.getTokenAuthen(tokenTxt);
+    // config.headers.Authorization =`Bearer ${response}`;
+}
 return config;
 })
 axiosClient.interceptors.response.use((response) => {
